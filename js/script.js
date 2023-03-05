28fd1215c462b53fbd7d237570b9a4440c088f09
davigledson <davigledson@outlook.com>
2023-03-05 12:40:15 -0300

    função do menu esquerdo feita

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ searchIcon.addEventListener('click', function() {
 
 containerSearch.classList.toggle('container-active') //document.body.classList.toggle('input-search') - altenaria a classe no body
 });
+
+
+const arrowLeft = document.getElementById('arrow-left')
+const leftContainerActive = document.getElementById('left-menu-container-active')
+
+arrowLeft.addEventListener('click',function(){
+leftContainerActive.classList.toggle('left-menu-container-active')
+})
